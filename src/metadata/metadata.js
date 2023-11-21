9a501390e22bee916673df660385d8d61d8c597d
MarcoParola <marcoparola96@gmail.com>
2023-11-21 22:36:54 +0100

    update cv: add education and bio

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,13 +1,9 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Marco Parola',
+  title: 'Ph.D candidate',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Twitter': `<a href="https://github.com/MarcoParola"><i class="fa-brands fa-github fact-icon"></i>Shakespeare</a>`,
+    'Email': '<a href="mailto:marco.parola@ing.unipi.it"><i class="fa fa-envelope fact-icon"></i>marco.parola@ing.unipi.it</a>'
   },
   skills: [
     ['Comedy', 100],
