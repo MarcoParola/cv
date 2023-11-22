4f8db90a218d9089b041190201d662a3ca96531a
MarcoParola <marcoparola96@gmail.com>
2023-11-22 17:24:13 +0100

    update cv: add publications to publication section

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -5,7 +5,7 @@ module.exports = {
     'Linkedin': `<a href="https://www.linkedin.com/in/marco-parola-5269a1121/"><i class="fa-brands fa-linkedin fact-icon" style="color: #373737;"></i></a>`,
     'Telegram': `<a href="https://telegram.me/parola_marco"><i class="fa-brands fa-telegram fact-icon" style="color: #373737;"></i></a>`,
     'GitHub': `<a href="https://github.com/MarcoParola"><i class="fa-brands fa-github fact-icon" style="color: #373737;"></i></a>`,
-    'Stack Overflow': `<a href="https://stackoverflow.com/users/22515567/marco-parola"><i class="fa-brands fa-stack-overflow fact-icon" style="color: #373737;"></i></a>`,
+    'Stack Overflow': `<a href="https://stackoverflow.com/users/22515567/marco-parola"><i class="fa-brands fa-stack-overflow fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
     'Email': '<a href="mailto:marco.parola@ing.unipi.it"><i class="fa fa-envelope fact-icon" style="color: #373737;"></i></a>'
   },
   skills: [
@@ -26,25 +26,25 @@ module.exports = {
       title: 'Ph.D. candidate',
       period: '01/11/2022-present',
       skills: ['Deep Learning', 'XAI', 'Medical Imaging', 'Computer Vision'],
-      contents: 'Ph.D. candidate enrolled in  Information Engineering phd program at University of Pisa'
+      contents: 'Ph.D. candidate enrolled in  Information Engineering phd program at [University of Pisa](https://www.dii.unipi.it/).'
     },
     {
       title: 'Research fellow',
       period: '01/05/2022-31/10/2022 ',
       skills: ['Deep Learning', 'XAI', 'Medical Imaging', 'Computer Vision'],
-      contents: 'Research fellow on "Decision support systems with explainable models for diagnosis and prognosis based on medical images" funded by MUR-PRIN REASONIG 2020 project, supervisors Prof. Gigliola Vaglini and Prof. Mario GCA Cimino; at University of Pisa.'
+      contents: 'Research fellow on "Decision support systems with explainable models for diagnosis and prognosis based on medical images" funded by MUR-PRIN REASONIG 2020 project, supervisors Prof. Gigliola Vaglini and Prof. Mario GCA Cimino; at [University of Pisa](https://www.dii.unipi.it/).'
     },
     {
       title: 'Machine learning engineer',
       period: '15/10/2021-30/04/2022',
       skills: ['Python', 'Flask', 'MongoDB', 'Signal processing', 'Sensors'],
-      contents: 'Machine learning engineer at <a href="https://www.movesolutions.it">Move Solutions</a> - Move S.r.l., Lucca. Part time. I developed microservices to be integrated into the Move Solutions platform architecture to provide ML analysis on sensor signals.'
+      contents: 'Machine learning engineer at [Move Solutions](https://www.movesolutions.it) - Move S.r.l., Lucca. Part time. I developed microservices to be integrated into the Move Solutions platform architecture to provide ML analysis on sensor signals.'
     },
     {
       title: 'Software developer ',
       period: '01/09/2019-15/06/2021',
       skills: ['.NET', 'ASP.NET', 'MSSQL', 'C#', 'web'],
-      contents: 'Software developer at (5Space)[https://www.5space.it] S.n.c., Livorno. I worked part-time on OptGear project, a web application to perform optimization calculations for mechanical gears, which finds the best combination of gear parameters.'
+      contents: 'Software developer at [5Space](https://www.5space.it) S.n.c., Livorno. I worked part-time on OptGear project, a web application to perform optimization calculations for mechanical gears, which finds the best combination of gear parameters.'
     },
     {
       title: 'Software developer intern',
@@ -73,8 +73,37 @@ module.exports = {
   publication: [
     {
       type: 'C',
+      year: '2023',
+      code: 'https://github.com/MarcoParola/detection_framework',
+      skills: ['Object Detection', 'Medical Imaging', 'Deep Leraning', 'Ensemble Learning', 'Oral Cancer'],
+      contents: 'Parola, M., La Mantia, G., Galatolo, F., Cimino, M.G., Campisi, G., Di Fede, O., 2023. Image-based screening of oral cancer via deep ensemble architecture, in: 2023 IEEE Symposium Series on Computational Intelligence (SSCI), IEEE. pp. 1570–1576. doi:10.13140/RG.2.2.24070.34880'
+    },
+    {
+      type: 'C',
+      year: '2023',
+      code: 'https://github.com/GiadaSimionato/HDHSim',
+      skills: ['Drone', 'Swarm Intelligence', 'Hole Detection'],
+      contents: 'Simionato, G.; Parola, M.; and Cimino, M.; (2023). Impressionist Hole Detection and Healing Using Swarms of Agents with Quantized Perception. In Proceedings of 2023 IEEE Symposium Series on Computational Intelligence. (2023).'
+    },
+    {
+      type: 'C',
+      year: '2023',
+      code: 'https://github.com/MarcoParola/torre-clima',
+      skills: ['Regression', 'Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Tower of Pisa'],
+      contents: 'Parola, M.; Dirrhami, H.; Cimino, M. and Squeglia, N. (2023). Effects of Environmental Conditions on Historic Buildings: Interpretable Versus Accurate Exploratory Data Analysis. In Proceedings of the 12th International Conference on Data Science, Technology and Applications - DATA; ISBN 978-989-758-664-4; ISSN 2184-285X, SciTePress, pages 429-435. DOI: 10.5220/0012119700003541'
+    },
+    {
+      type: 'C',
       year: '2022',
-      git: 'https://github.com/MarcoParola/structural_health_monitoring',
+      code: 'https://github.com/galatolofederico/torre-transformer',
+      skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Tower of Pisa'],
+      contents: 'Cimino, M.; Galatolo, F.; Parola, M.; Perilli, N. and Squeglia, N. (2022). Deep Learning of Structural Changes in Historical Buildings: The Case Study of the Pisa Tower. In Proceedings of the 14th International Joint Conference on Computational Intelligence (IJCCI 2022) - NCTA; ISBN 978-989-758-611-8; ISSN 2184-3236, SciTePress, pages 396-403. DOI: 10.5220/0011551800003332'
+    },
+    {
+      type: 'C',
+      year: '2022',
+      code: 'https://github.com/MarcoParola/structural_health_monitoring',
+      data: 'http://131.114.50.176/owncloud/s/TZ1G53X18PevT1b/download',
       skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Digital Twin'],
       contents: 'Parola, M.; Galatolo, F.; Torzoni, M.; Cimino, M. and Vaglini, G. (2022). Structural Damage Localization via Deep Learning and IoT Enabled Digital Twin. In Proceedings of the 3rd International Conference on Deep Learning Theory and Applications - DeLTA; ISBN 978-989-758-584-5; ISSN 2184-9277, SciTePress, pages 199-206. DOI: 10.5220/0011320600003277'
     }
