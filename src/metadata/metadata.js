a7630b0aa400a07014150bf059e4afab3be98c91
MarcoParola <marcoparola96@gmail.com>
2023-11-23 12:17:20 +0100

    adjust formatting

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -82,21 +82,21 @@ module.exports = {
       year: '2023',
       code: 'https://github.com/MarcoParola/detection_framework',
       skills: ['Object Detection', 'Medical Imaging', 'Deep Leraning', 'Ensemble Learning', 'Oral Cancer'],
-      contents: 'Parola, M., La Mantia, G., Galatolo, F., Cimino, M.G., Campisi, G., Di Fede, O., 2023. Image-based screening of oral cancer via deep ensemble architecture, in: 2023 IEEE Symposium Series on Computational Intelligence (SSCI), IEEE. pp. 1570–1576. doi:10.13140/RG.2.2.24070.34880'
+      contents: '**Parola, M.**, La Mantia, G., Galatolo, F., Cimino, M.G., Campisi, G., Di Fede, O., 2023. **Image-based screening of oral cancer via deep ensemble architecture**, in: 2023 IEEE Symposium Series on Computational Intelligence (SSCI), IEEE. pp. 1570–1576. doi:10.13140/RG.2.2.24070.34880'
     },
     {
       type: 'C',
       year: '2023',
       code: 'https://github.com/GiadaSimionato/HDHSim',
       skills: ['Drone', 'Swarm Intelligence', 'Hole Detection'],
-      contents: 'Simionato, G.; Parola, M.; and Cimino, M.; (2023). Impressionist Hole Detection and Healing Using Swarms of Agents with Quantized Perception. In Proceedings of 2023 IEEE Symposium Series on Computational Intelligence. (2023).'
+      contents: 'Simionato, G.; **Parola, M.**; and Cimino, M.; (2023). **Impressionist Hole Detection and Healing Using Swarms of Agents with Quantized Perception**. In Proceedings of 2023 IEEE Symposium Series on Computational Intelligence. (2023).'
     },
     {
       type: 'C',
       year: '2023',
       code: 'https://github.com/MarcoParola/torre-clima',
       skills: ['Regression', 'Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Tower of Pisa'],
-      contents: 'Parola, M.; Dirrhami, H.; Cimino, M. and Squeglia, N. (2023). Effects of Environmental Conditions on Historic Buildings: Interpretable Versus Accurate Exploratory Data Analysis. In Proceedings of the 12th International Conference on Data Science, Technology and Applications - DATA; ISBN 978-989-758-664-4; ISSN 2184-285X, SciTePress, pages 429-435. DOI: 10.5220/0012119700003541'
+      contents: '**Parola, M.**; Dirrhami, H.; Cimino, M. and Squeglia, N. (2023). **Effects of Environmental Conditions on Historic Buildings: Interpretable Versus Accurate Exploratory Data Analysis**. In Proceedings of the 12th International Conference on Data Science, Technology and Applications - DATA; ISBN 978-989-758-664-4; ISSN 2184-285X, SciTePress, pages 429-435. DOI: 10.5220/0012119700003541'
     },
     {
       type: 'O',
@@ -104,14 +104,14 @@ module.exports = {
       code: 'https://github.com/MarcoParola/structural_health_monitoring',
       data: 'http://131.114.50.176/owncloud/s/TZ1G53X18PevT1b/download',
       skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Digital Twin'],
-      contents: 'Parola, M.; Galatolo, F.; Torzoni, M. and Cimino, M. (2022). Convolutional Neural Networks for Structural Damage Localization on Digital Twins. In International Conference on Deep Learning Theory and Applications (pp. 78-97). Cham: Springer Nature Switzerland.'
+      contents: '**Parola, M.**; Galatolo, F.; Torzoni, M. and Cimino, M. (2022). **Convolutional Neural Networks for Structural Damage Localization on Digital Twins**. In International Conference on Deep Learning Theory and Applications (pp. 78-97). Cham: Springer Nature Switzerland.'
     },
     {
       type: 'C',
       year: '2022',
       code: 'https://github.com/galatolofederico/torre-transformer',
       skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Tower of Pisa'],
-      contents: 'Cimino, M.; Galatolo, F.; Parola, M.; Perilli, N. and Squeglia, N. (2022). Deep Learning of Structural Changes in Historical Buildings: The Case Study of the Pisa Tower. In Proceedings of the 14th International Joint Conference on Computational Intelligence (IJCCI 2022) - NCTA; ISBN 978-989-758-611-8; ISSN 2184-3236, SciTePress, pages 396-403. DOI: 10.5220/0011551800003332'
+      contents: 'Cimino, M.; Galatolo, F.; **Parola, M.**; Perilli, N. and Squeglia, N. (2022). **Deep Learning of Structural Changes in Historical Buildings: The Case Study of the Pisa Tower**. In Proceedings of the 14th International Joint Conference on Computational Intelligence (IJCCI 2022) - NCTA; ISBN 978-989-758-611-8; ISSN 2184-3236, SciTePress, pages 396-403. DOI: 10.5220/0011551800003332'
     },
     {
       type: 'C',
@@ -119,13 +119,13 @@ module.exports = {
       code: 'https://github.com/MarcoParola/structural_health_monitoring',
       data: 'http://131.114.50.176/owncloud/s/TZ1G53X18PevT1b/download',
       skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Digital Twin'],
-      contents: 'Parola, M.; Galatolo, F.; Torzoni, M.; Cimino, M. and Vaglini, G. (2022). Structural Damage Localization via Deep Learning and IoT Enabled Digital Twin. In Proceedings of the 3rd International Conference on Deep Learning Theory and Applications - DeLTA; ISBN 978-989-758-584-5; ISSN 2184-9277, SciTePress, pages 199-206. DOI: 10.5220/0011320600003277'
+      contents: '**Parola, M.**; Galatolo, F.; Torzoni, M.; Cimino, M. and Vaglini, G. (2022). **Structural Damage Localization via Deep Learning and IoT Enabled Digital Twin**. In Proceedings of the 3rd International Conference on Deep Learning Theory and Applications - DeLTA; ISBN 978-989-758-584-5; ISSN 2184-9277, SciTePress, pages 199-206. DOI: 10.5220/0011320600003277'
     },
     {
       type: 'O',
       year: '2021',
       skills: ['Information Retrieval', 'Computer Vision', 'Deep Leraning'],
-      contents: 'Parola, M., Nannini, A., & Poleggi, S. (2021). Web image search engine based on LSH index and CNN Resnet50. arXiv preprint arXiv:2108.13301.'
+      contents: '**Parola, M.**, Nannini, A., & Poleggi, S. (2021). **Web image search engine based on LSH index and CNN Resnet50**. arXiv preprint arXiv:2108.13301.'
     }
   ]
 };
