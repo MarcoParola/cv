d0da28133ca289578ab1a5668e08cfe401fd461b
MarcoParola <marcoparola96@gmail.com>
2023-11-23 11:32:17 +0100

    update cd: rewrite bio

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,12 +1,14 @@
 module.exports = {
   name: 'Marco Parola',
   title: 'Ph.D. candidate',
-  facts: {
-    'Linkedin': `<a href="https://www.linkedin.com/in/marco-parola-5269a1121/"><i class="fa-brands fa-linkedin fact-icon" style="color: #373737;"></i></a>`,
-    'Telegram': `<a href="https://telegram.me/parola_marco"><i class="fa-brands fa-telegram fact-icon" style="color: #373737;"></i></a>`,
-    'GitHub': `<a href="https://github.com/MarcoParola"><i class="fa-brands fa-github fact-icon" style="color: #373737;"></i></a>`,
+  links: {
+    'Linkedin': `<a href="https://www.linkedin.com/in/marco-parola-5269a1121/"><i class="fa-brands fa-linkedin fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
+    'Telegram': `<a href="https://telegram.me/parola_marco"><i class="fa-brands fa-telegram fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
+    'GitHub': `<a href="https://github.com/MarcoParola"><i class="fa-brands fa-github fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
     'Stack Overflow': `<a href="https://stackoverflow.com/users/22515567/marco-parola"><i class="fa-brands fa-stack-overflow fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
-    'Email': '<a href="mailto:marco.parola@ing.unipi.it"><i class="fa fa-envelope fact-icon" style="color: #373737;"></i></a>'
+    'ORCID': `<a href="https://orcid.org/0000-0003-4871-490"><i class="fa-brands fa-orcid fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
+    'ResearchGate': `<a href="https://www.researchgate.net/profile/Marco-Parola"><i class="fa-brands fa-researchgate fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>`,
+    'Email': '<a href="mailto:marco.parola@ing.unipi.it"><i class="fa fa-envelope fact-icon" style="color: #373737; font-size: 1.5em;"></i></a>'
   },
   skills: [
     ['Python', 100],
@@ -24,31 +26,31 @@ module.exports = {
   positions: [
     {
       title: 'Ph.D. candidate',
-      period: '01/11/2022-present',
+      period: '01/11/2022 - present',
       skills: ['Deep Learning', 'XAI', 'Medical Imaging', 'Computer Vision'],
       contents: 'Ph.D. candidate enrolled in  Information Engineering phd program at [University of Pisa](https://www.dii.unipi.it/).'
     },
     {
       title: 'Research fellow',
-      period: '01/05/2022-31/10/2022 ',
+      period: '01/05/2022 - 31/10/2022 ',
       skills: ['Deep Learning', 'XAI', 'Medical Imaging', 'Computer Vision'],
       contents: 'Research fellow on "Decision support systems with explainable models for diagnosis and prognosis based on medical images" funded by MUR-PRIN REASONIG 2020 project, supervisors Prof. Gigliola Vaglini and Prof. Mario GCA Cimino; at [University of Pisa](https://www.dii.unipi.it/).'
     },
     {
       title: 'Machine learning engineer',
-      period: '15/10/2021-30/04/2022',
-      skills: ['Python', 'Flask', 'MongoDB', 'Signal processing', 'Sensors'],
+      period: '15/10/2021 - 30/04/2022',
+      skills: ['Python', 'Flask', 'MongoDB', 'Signal processing', 'Sensors', 'IoT', 'microservices', 'Docker'],
       contents: 'Machine learning engineer at [Move Solutions](https://www.movesolutions.it) - Move S.r.l., Lucca. Part time. I developed microservices to be integrated into the Move Solutions platform architecture to provide ML analysis on sensor signals.'
     },
     {
       title: 'Software developer ',
-      period: '01/09/2019-15/06/2021',
-      skills: ['.NET', 'ASP.NET', 'MSSQL', 'C#', 'web'],
+      period: '01/09/2019 - 15/06/2021',
+      skills: ['.NET', 'ASP.NET', 'MSSQL', 'C#', 'web', 'microservices'],
       contents: 'Software developer at [5Space](https://www.5space.it) S.n.c., Livorno. I worked part-time on OptGear project, a web application to perform optimization calculations for mechanical gears, which finds the best combination of gear parameters.'
     },
     {
       title: 'Software developer intern',
-      period: '30/07/2018-31/01/2019',
+      period: '30/07/2018 - 31/01/2019',
       skills: ['.NET', 'ASP.NET', 'MSSQL', 'C#', 'web'],
       contents: 'Software developer at [Labortori Archa](https://www.archa.it) S.r.l., Pisa. Internship. Implementation of LIMS (Laboratory Information Management System) using Microsoft technology stack.'
     }
@@ -93,6 +95,14 @@ module.exports = {
       contents: 'Parola, M.; Dirrhami, H.; Cimino, M. and Squeglia, N. (2023). Effects of Environmental Conditions on Historic Buildings: Interpretable Versus Accurate Exploratory Data Analysis. In Proceedings of the 12th International Conference on Data Science, Technology and Applications - DATA; ISBN 978-989-758-664-4; ISSN 2184-285X, SciTePress, pages 429-435. DOI: 10.5220/0012119700003541'
     },
     {
+      type: 'O',
+      year: '2022',
+      code: 'https://github.com/MarcoParola/structural_health_monitoring',
+      data: 'http://131.114.50.176/owncloud/s/TZ1G53X18PevT1b/download',
+      skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Digital Twin'],
+      contents: 'Parola, M.; Galatolo, F.; Torzoni, M. and Cimino, M. (2022). Convolutional Neural Networks for Structural Damage Localization on Digital Twins. In International Conference on Deep Learning Theory and Applications (pp. 78-97). Cham: Springer Nature Switzerland.'
+    },
+    {
       type: 'C',
       year: '2022',
       code: 'https://github.com/galatolofederico/torre-transformer',
@@ -106,6 +116,12 @@ module.exports = {
       data: 'http://131.114.50.176/owncloud/s/TZ1G53X18PevT1b/download',
       skills: ['Stuctural Health Monitoring', 'Deep Leraning', 'Sensors', 'Digital Twin'],
       contents: 'Parola, M.; Galatolo, F.; Torzoni, M.; Cimino, M. and Vaglini, G. (2022). Structural Damage Localization via Deep Learning and IoT Enabled Digital Twin. In Proceedings of the 3rd International Conference on Deep Learning Theory and Applications - DeLTA; ISBN 978-989-758-584-5; ISSN 2184-9277, SciTePress, pages 199-206. DOI: 10.5220/0011320600003277'
+    },
+    {
+      type: 'O',
+      year: '2021',
+      skills: ['Information Retrieval', 'Computer Vision', 'Deep Leraning'],
+      contents: 'Parola, M., Nannini, A., & Poleggi, S. (2021). Web image search engine based on LSH index and CNN Resnet50. arXiv preprint arXiv:2108.13301.'
     }
   ]
 };
